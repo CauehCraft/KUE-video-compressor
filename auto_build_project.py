3bd4a88854dc1907d9100b06e68a8397b3cb4530
Caueh Oliveira <12563720+CauehCraft@users.noreply.github.com>
2023-06-24 01:00:46 -0300

    Update auto_build_project.py

--- auto_build_project.py
+++ auto_build_project.py
@@ -1,14 +1,26 @@
 import sys
 import subprocess
 
+def get_package_location(package_name):
+    result = subprocess.run([sys.executable, "-m", "pip", "show", package_name], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    for line in output.split('\n'):
+        if line.startswith('Location:'):
+            return line.split()[-1]
+    return None
+
 # Caminho para o arquivo requirements.txt
 requirements_path = "requirements.txt"
 
 # Instalar as dependências do Pip a partir do arquivo requirements.txt
 subprocess.run([sys.executable, "-m", "pip", "install", "-r", requirements_path])
 
-# Caminho para o arquivo tkinterdnd2, altere caso necessario.
-tkinterdnd2_path = "C:/Users/Caueh/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0/LocalCache/local-packages/Python311/site-packages/tkinterdnd2"
+# Obter o caminho de instalação das bibliotecas tkinterdnd2 e customtkinter
+tkinterdnd2_path = get_package_location("tkinterdnd2")+"\\tkinterdnd2"
+customtkinter_path = get_package_location("customtkinter")+"\\customtkinter"
+
+print(tkinterdnd2_path)
+print(customtkinter_path)
 
 # Executar o comando PyInstaller para criar o executável
 subprocess.run([
@@ -18,6 +30,6 @@ subprocess.run([
     "--add-data", f"imgs;imgs",
     "--add-data", f"ffmpeg;ffmpeg",
     "--add-data", f"{tkinterdnd2_path};tkinterdnd2",
-    "--onefile",
-    "main.py"
+    "--add-data", f"{customtkinter_path};customtkinter/",
+    "KUE-VEC.py"
 ])
